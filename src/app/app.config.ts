ba8912ddfc2c9ef81e389af87091174ac86a8e67
oussama <oukhayloussama@gmail.com>
2024-01-18 21:37:19 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,43 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {KeycloakService} from "keycloak-angular";
+
+import {HTTP_INTERCEPTORS,  HttpClientModule} from "@angular/common/http";
+import {AuthInterceptorService} from "./services/auth.service";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'ouss',
+        clientId: 'frontend'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),HttpClientModule,KeycloakService,
+
+    {
+    provide: APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+
+    multi: true,
+    deps: [KeycloakService],
+  },{
+      provide: HTTP_INTERCEPTORS,
+      useClass:AuthInterceptorService,
+      multi: true,
+    }
+
+
+  ]
 };
